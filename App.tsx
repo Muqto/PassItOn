21e732237d660fd32d36d6209d8ba0657f5e5050
Muqto <muqto14@gmail.com>
2024-02-13 16:22:39 -0500

    clean project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from "react-native";
+import Signin from "./Components/Signup";
+import Signup from "./Components/Signup";
 
-export default function App() {
+export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <View style = {styles.container}>
+        <Signup/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
