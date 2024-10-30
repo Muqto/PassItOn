f5c4418955abe526c3c47821098a4c5318c54531
miiyu-fujita <71932936+miiyu-fujita@users.noreply.github.com>
2024-10-30 15:43:48 -0400

    add env file back for now

---
+++ env.js
@@ -0,0 +1,3 @@
+export const LOCALHOST_IP = "http://10.122.247.105:6006/"
+export const GOOGLE_PLACES_API_KEY = "AIzaSyA21klGkCwvV74Eu4ptZBbxlHkVlMUorSE"
+export const FIREBASE_API_KEY = "AIzaSyC7kGZPrlHio1wHaq37slKXRj2sJCCfA5A"
